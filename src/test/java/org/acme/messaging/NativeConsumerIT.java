a7d1e177922d50a681672be7194e11284d5eea0c
Mike Hepburn <eformat@gmail.com>
2019-07-03 20:04:54 +1000

    initial commit

--- src/test/java/org/acme/messaging/NativeConsumerIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.messaging;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeConsumerIT extends ConsumerTest {
-
-    // Execute the same tests but in native mode.
-}
